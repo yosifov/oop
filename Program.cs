9a1b855083e4789b429c85b14cd46948d4b0abd8
Yosifov <k.yosifov@gmail.com>
2019-09-27 23:07:47 +0300

    CSharp OOP initial install

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("CSharp OOP");
+        }
+    }
+}
