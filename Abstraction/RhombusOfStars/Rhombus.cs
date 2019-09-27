a7d200721b564660af8a45f7fd11871d3021a4f4
Yosifov <k.yosifov@gmail.com>
2019-09-27 23:35:40 +0300

    Add Rhombus of Stars task
    Task for Abstraction Lab
    
    closes #1

---
+++ Abstraction/RhombusOfStars/Rhombus.cs
@@ -0,0 +1,40 @@
+﻿namespace OOP.Abstraction.RhombusOfStars
+{
+    using System;
+    using System.Text;
+
+    public class Rhombus
+    {
+        public static void Execute()
+        {
+            int count = int.Parse(Console.ReadLine());
+            PrintRhombus(count);
+        }
+
+        private static void PrintRhombus(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                PrintRhombusRow(i, count);
+            }
+            for (int i = count - 1; i > 0; i--)
+            {
+                PrintRhombusRow(i, count);
+            }
+        }
+
+        private static void PrintRhombusRow(int stars, int totalStars)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(new string(' ', totalStars - stars));
+
+            for (int i = 1; i <= stars; i++)
+            {
+                sb.Append("* ");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("CSharp OOP");
+            Abstraction.RhombusOfStars.Rhombus.Execute();
         }
     }
 }
