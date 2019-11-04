b4face7ab04bcc90c74f0e6b163a47cd02142ca2
Yosifov <k.yosifov@gmail.com>
2019-11-04 23:51:30 +0200

    Add Class Box Data Validation Task
    Task for Encapsulation Exercise
    
    closes #7

--- Encapsulation/ClassBox/Box.cs
+++ Encapsulation/ClassBox/Box.cs
@@ -1,5 +1,6 @@
 ﻿namespace OOP.Encapsulation.ClassBox
 {
+    using System;
     using System.Text;
 
     public class Box
@@ -10,9 +11,42 @@
 
         public Box(double length, double width, double height)
         {
-            this.length = length;
-            this.width = width;
-            this.height = height;
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length 
+        {
+            get => this.length;
+            private set
+            {
+                ValidateSide(value);
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set 
+            {
+                ValidateSide(value);
+
+                this.width = value;
+            }
+        }
+
+        public double Height 
+        {
+            get => this.height; 
+            set
+            {
+                ValidateSide(value);
+
+                this.height = value;
+            }
         }
 
         public double GetSurfaceArea()
@@ -46,6 +80,14 @@
             sb.AppendLine($"Lateral Surface Area - {this.GetLateralSurfaceArea():F2}");
             sb.Append($"Volume - {this.GetVolume():F2}");
             return sb.ToString();
+        }
+
+        private static void ValidateSide(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Box side cannot be zero or negative.");
+            }
         }
     }
 }
