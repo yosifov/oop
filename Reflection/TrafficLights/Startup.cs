aba9c5317fa5e4f8f1874620404fc1e167bbab6f
Yosifov <k.yosifov@gmail.com>
2019-12-27 00:25:18 +0200

    Add Traffic Lights task for reflection exercise

--- Reflection/TrafficLights/Startup.cs
+++ Reflection/TrafficLights/Startup.cs
@@ -1,12 +1,23 @@
 ﻿namespace OOP.Reflection.TrafficLights
 {
     using System;
+    using System.Linq;
 
     public class Startup : IService
     {
         public void Execute()
         {
-            throw new NotImplementedException();
+            var trafficLights = Console.ReadLine().Trim().Split()
+                .Select(x => new TrafficLight(x))
+                .ToList();
+
+            var n = int.Parse(Console.ReadLine().Trim());
+
+            for (int i = 0; i < n; i++)
+            {
+                trafficLights.ForEach(t => t.ToggleSignal());
+                Console.WriteLine(string.Join(" ", trafficLights));
+            }
         }
     }
 }
---
+++ Reflection/TrafficLights/TrafficLight.cs
@@ -0,0 +1,36 @@
+﻿namespace OOP.Reflection.TrafficLights
+{
+    internal class TrafficLight
+    {
+        internal enum TrafficSignal
+        {
+            Red = 0,
+            Green = 1,
+            Yellow = 2
+        }
+
+        private TrafficSignal signal;
+
+        public string Signal
+        {
+            get => this.signal.ToString();
+            private set { TrafficSignal.TryParse(value, out this.signal); }
+        }
+
+        public void ToggleSignal()
+        {
+            var signals = typeof(TrafficSignal).GetEnumValues();
+            this.signal = (TrafficSignal)signals.GetValue((int)(signal + 1) % signals.Length);
+        }
+
+        public override string ToString()
+        {
+            return this.Signal;
+        }
+
+        public TrafficLight(string signal)
+        {
+            this.Signal = signal;
+        }
+    }
+}
