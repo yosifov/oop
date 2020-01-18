9e28fb52c27106aa61df92294b7d923f8adcf682
Yosifov <k.yosifov@gmail.com>
2020-01-18 18:39:45 +0200

    Fix EventImplementation task

--- Events/EventImplementation/Dispatcher.cs
+++ Events/EventImplementation/Dispatcher.cs
@@ -2,22 +2,27 @@
 {
     using System;
 
-    public delegate void NameChangeEventHandler();
+    public delegate void NameChangeEventHandler(object sender, NameChangeEventArgs args);
 
     public class Dispatcher
     {
         private string name;
 
-        public event EventHandler<NameChangeEventArgs> NameChange;
+        public event NameChangeEventHandler NameChange;
 
         public string Name
         {
             get => this.name;
             set
             {
-                this.NameChange?.Invoke(this, new NameChangeEventArgs(value));
+                this.OnNameChange(new NameChangeEventArgs(value));
                 this.name = value;
             }
+        }
+
+        public void OnNameChange(NameChangeEventArgs args)
+        {
+            this.NameChange?.Invoke(this, args);
         }
     }
 }
